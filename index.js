44eca53a9f30c02fa01fcd2812d75c4af590f036
NatalyaSemenova <ambernou@gmail.com>
2022-08-30 18:56:45 +0700

    get

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const path = require('path')
 const serveStatic = require('serve-static')
+const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 
-app.use('/', serveStatic(path.join(__dirname, 'vue-crm', 'dist')))
+app.use(cors)
+app.use(express.json())
+//app.use('/', serveStatic(path.join(__dirname, 'vue-crm', 'dist')))
+app.use(express.urlencoded({ extended: true }))
 
-const port = process.env.PORT || 3000
+app.post('/crm', (req, res) => {
+    console.log('запрос', req.url)
+    // res.send(await axios.get('https://ambernou.amocrm.ru/api/v4/leads', { headers: { "Authorization" : "Bearer  eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImU5YWI0YmU3ODg4YTMxMWQzZDA5MmRmZGI5ZjY1MDRmMmQ1MDA4MjI0M2M4N2QzY2I3MDA0MGRjYjRmMGIzODEyZDZiYWE0MmZmOGJlNjFlIn0.eyJhdWQiOiJkNmQ4OWIxMS04MWNlLTRkZGUtYjU5Yy1iZjQwZGZjMGFlZWIiLCJqdGkiOiJlOWFiNGJlNzg4OGEzMTFkM2QwOTJkZmRiOWY2NTA0ZjJkNTAwODIyNDNjODdkM2NiNzAwNDBkY2I0ZjBiMzgxMmQ2YmFhNDJmZjhiZTYxZSIsImlhdCI6MTY2MTg1MDYwNCwibmJmIjoxNjYxODUwNjA0LCJleHAiOjE2NjE5MzcwMDQsInN1YiI6Ijg1MDM4OTEiLCJhY2NvdW50X2lkIjozMDM2NjE2OSwic2NvcGVzIjpbInB1c2hfbm90aWZpY2F0aW9ucyIsImNybSIsIm5vdGlmaWNhdGlvbnMiXX0.riiBsaTZxdjjxS3WeXMQJxd90ELw4LX1cYlpwpKsW3VnpcdlTU2fhBEXMZ4gTrEDFwy8C_UK9NEzml6itrw3-8byivgQaMaEcAR46UwFJEo7JBeBEOqSjjXRsTg-43EdTUb0u3MI96wrj91jAlwwaLDQ5eHKN0G1-kLcD8Fk8FAn4jgNNlv7jwyHkPXglkN3lYHgs8AflIKMt6N8Gu6dE5iICsoS9KmLDn8iGdFRj8wj5aD23Fmyr0o_5YNWQPVZVppEmqjqwhX8T9UuYGobLLs-gTg_hBa4e7qBkGeCUl9YMmtxn8s0ZE-LToukj1JeV3RlY5mdmOYi3bhIWbAjdQ"}})
+    //     .then(response => {
+    //         console.log('response ', response.data)
+    //         return response    
+    //     })
+    // )
+    res.send('answer')
+})
+
+const port = 3000
 
 app.listen(port)
--- vue-crm/src/App.vue
+++ vue-crm/src/App.vue
@@ -1,47 +1,28 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      data: ''
+    }
+  },
+  mounted() {
+    console.log('create')
+    axios.post('http://localhost:3000/crm', '123')
+      .then(res => console.log('response: ', res))
+      .catch(e => console.log(e))
+    console.log('123')
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h3>get rcm</h3>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
